06da8a2e6af3e1b66110c56ba6ab4cbd92edcb5b
Thomas Skubicki <tskubicki@gmail.com>
2016-07-13 00:21:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+discord-xmpp
+===================
+
+A bot for Discord that transfers chat messages between a Discord channel and a XMPP-based Multi-user Chatroom (MUC). It runs on node.js, and utilizes discord.js, node-xmpp-client, and tinyurl. 
+
+**Installation**
+-------------
+
+**You need all of the following before you start:**
+> - A working installation of Node.js on the machine this bot will run on. You can get it at https://nodejs.org
+> - Access to the Discord account that created the server the Discord chatroom lives on.
+> - The ID number of the Discord chatroom the bot will listen to. You can get it by logging into the web version of Discord at "discord.gg",  and browsing to the chatroom. It's in the URL as the last number, like the following example: https://discordapp.com/channels/SERVER_ID/CHATROOM_ID
+> - An account on the XMPP server that the bot will run under, also known as a JID (Jabber ID). It's usually something like: "username@XmppServerName"
+> - The JID of the chatroom on your XMPP server. It tends to look like: "chatroom@conference.XmppServerName"
+
+**Now, lets begin**
+
+1. Log into the Discord Developer's site at "https://discordapp.com/developers/applications/me" using the server owner's account.
+2. Click on "New Application"
+3. Type a meaningful name under "APP NAME*". This is name of the bot-user XMPP chat will appear from.
+4. If you want, click on "App Icon" to upload an image for your bot to use as an avatar.
+5. When done, click "Create Application" at the bottom of the page.
+6. At the next page, click on the "click to reveal" link under "App Details"
+7. Click on the "Create a Bot User" button, and then the "Yes. Do it!" button.
+8. Under "App Bot User", click on the "click to reveal" link, and copy that token somewhere temporarily. You will need it later.
+9. Click 'Save Changes'
+10. Under "App Details", copy the "Client/Application ID" and put it somewhere for a moment, you'll need it for the next step
+11. Change the following URL to use the Client ID you just copied, and browse to it: https://discordapp.com/oauth2/authorize?client_id=PUT_YOUR_CLIENT_ID_HERE&scope=bot&permissions=0
+12. Click on the dropdown box under "Add a bot to a server", select the server your chatroom is in, and click the "Authorize" button.
+13. We're now done with the Discord side. Now to setup the bot
+
+14. Clone this repo to your desired folder
+15. Open your command line and browse to the folder
+16. Type "npm install --save" and press enter. This may take a few moments to complete
+17. Open "app.js" using a text editor, and turn your attention to first five lines.
+18. On line 1, replace 'YOUR_DISCORD_API_TOKEN' with the token you got earlier. Make sure to keep it in 'quotes'
+19. On line 2, replace 'YOUR_DISCORD_CHATROOM_ID' with the id of the chatroom you want this to listen on. Again, make sure to keep it in 'quotes'
+20. On line 3, replace 'username@XmppServerName' with the JID you want to use.
+21. On line 4, replace 'YOUR_PASSWORD' with the password of the account you used on line 3.
+22. On line 5, replace 'chatroom@conference.XmppServerName' with the JID of the room you want this to listen on.
+23. Save your changes
+24. Open your command line and browse to the folder
+25. Type the command "node app.js" and press enter. If you get an error about "command not found" try using "nodejs app.js" instead.
+26. If its working, you will see two messages appear in your console:
+> "Successfully connected to XMPP chatroom"
+> "Successfully connected to Discord"
---
+++ app.js
@@ -0,0 +1,146 @@
+const DISCORD_API_TOKEN = 'YOUR_DISCORD_API_TOKEN';
+const DISCORD_CHATROOM_ID = 'YOUR_DISCORD_CHATROOM_ID';
+const JID = 'username@XmppServerName';
+const PASSWORD = 'YOUR_PASSWORD';
+const ROOM_JID = 'chatroom@conference.XmppServerName';
+
+//The name this bot will use in your XMPP Chatroom. 
+//You can change this to whatever you want
+const XMPP_ALIAS = '[DISCORD]'; 
+
+var Discord = require("discord.js");
+var XMPP = require('node-xmpp-client');
+var TinyURL = require ('tinyurl');
+
+//initialize node-xmpp-client
+var client = new XMPP({
+	jid: JID,
+	PASSWORD: PASSWORD,
+	preferred: 'PLAIN',
+	reconnect: true
+});
+
+//initialize discord.js
+var discord = new Discord.Client({autoreconnect: true});
+
+//auth with the Discord API with the above token
+discord.loginWithToken(DISCORD_API_TOKEN);
+
+//report that discord is connected
+discord.on('ready', function(){
+	console.log("Successfully connected to Discord.");
+});
+
+//report that XMPP is connected, and other things
+client.on('online', function() {
+	console.log("Successfully connected to XMPP chatroom '" + ROOM_JID + '\'.');
+
+	//let the world know we're online, and update our status accordingly
+	client.send(new XMPP.Stanza('presence', {type: 'available'}));
+
+	//join the Multi-User Chatroom
+	client.send(new XMPP.Element('presence', {from: JID, to: ROOM_JID + '/' + XMPP_ALIAS}));
+});
+
+//discord channel object representing our chatroom
+var channel = new Discord.Channel({
+	id: DISCORD_CHATROOM_ID,
+	client: "bot"
+});
+
+//handle messages from discord server
+discord.on("message", function(message) {
+
+	//ignore messages from the bot itself, and any messages except those from this channel
+	if (message.author.id === discord.user.id || 
+		  message.channel.id != DISCORD_CHATROOM_ID)
+		return;
+
+	var content = new String (message.content);
+	var isMe = false; //is this message using a '/me'
+
+  //handle messages with mentions
+	if (message.mentions.length){
+		message.mentions.forEach(function(mention){
+			content = content.replace(
+				new RegExp('<@(!|)' + mention.id + '>', 'g'),
+				'@' + mention.username
+			);
+		});		
+	}
+
+	//handle Discord '/me' command -- its represented by an underscore at the start and end of the content
+	if (message.content[0] == '_' && message.content[message.content.length - 1] == '_'){		
+		isMe = true;
+	  content = content.substr(1, content.length-2);	   
+	}
+	
+	if (message.attachments.length){
+		TinyURL.shorten(message.attachments[0].url,function(res){
+			client.send(new XMPP.Stanza('message', { to: ROOM_JID, type: 'groupchat' })
+			.c('body')
+			.t(message.author.username + 
+			  (isMe ? ' ' : ': ') +
+			  (message.attachments.length ? res + ' ' : '') + 
+				content));
+			});
+	}else{
+		client.send(new XMPP.Stanza('message', { to: ROOM_JID, type: 'groupchat' })
+		.c('body')
+		.t(message.author.username + 
+		  (isMe ? ' ' : ': ') +		  
+			content));
+	}
+
+});
+
+client.on('stanza', function(stanza) {
+	//ignore all history -- kinda dirty, but XMPP keeps throwing chat history stanzas despite telling it otherwise
+	//or I'm a n00b, likely the latter
+	var isHistory = false;
+	stanza.children.forEach(function(element){
+		if (element.name === 'delay')//if the name of any child element is 'delay', the stanza is chat history
+			isHistory = true;
+	});
+	if (isHistory) return;
+
+	// ignore everything that isn't from the chatroom, or came from the bot itself
+	if (stanza.is('message') &&
+	 	  stanza.attrs.type === 'groupchat' &&	   
+	 	  stanza.attrs.from !== ROOM_JID  + '/' + XMPP_ALIAS){
+
+		var body = stanza.getChild('body');
+
+		//stanzas with empty bodys are topic changes, and are ignored
+		if (!body) return;
+
+		//craft and send message 
+		var sender = stanza.attrs.from.split('/')[1];
+		var message = sender + ': ' + body.getText();
+		discord.sendMessage(channel, message); 
+	}
+});
+
+client.on('error', function(e) {
+	console.log("Something went wrong: ");
+	console.log(e);
+});
+
+//XMPP Server keep-alive
+setInterval(function(){
+	client.send(' ');
+}, 30000);
+
+
+//on close, set XMPP status to offline and logout of discord
+process.on('SIGINT', function(code) {
+	console.log('shutting down')
+	var stanza = new XMPP.Stanza('presence', {from: JID, to: ROOM_JID, type: 'unavailable'})
+	client.send(stanza)
+
+	discord.logout();
+
+	setTimeout(function() {
+		process.exit()
+	}, 1000);
+});
