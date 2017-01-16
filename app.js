c13072793d9983c987efffdc6b42f583410e1a20
Thomas Skubicki <tskubicki@gmail.com>
2017-01-16 15:30:33 -0500

    updated dependencies and added debug

--- app.js
+++ app.js
@@ -14,13 +14,31 @@ const RANDOM_ID = Math.random().toString(36).substring(2,10);
 //You can change this to whatever you want
 const IN_CHANNEL_BOT_NAME = '[DISCORD]'; 
 
+//Debugging options
+const DEBUG = false; //turn on the debug log. Useful to troubleshoot issues.
+const LOG_FILE = './debug.log'; //where the log file will be saved to. Same folder this file lives in by default 
+var xmppKeepAlivesSent = 0; //Keep track of XMPP keepAlives sent since the last message
+var fs = null;
+
 //Make libraries available
 var Discord = require("discord.js");
 var XMPP = require('node-xmpp-client');
 var TinyURL = require ('tinyurl');
 
+//Initialize Debug logging if enabled
+if (DEBUG){
+	fs = require('fs');
+	fs.access(LOG_FILE, function(err){
+		if (err){
+			console.log('\n' + formatLogEntry('No log file found at ' + LOG_FILE + '. Making a new one.'));
+			addLog('New log started');
+		}
+		addLog('Bot started');
+	});
+}
+
 //initialize node-xmpp-client
-var client = new XMPP({
+var xmpp = new XMPP({
 	jid: JID,
 	password: PASSWORD,
 	preferredSaslMechanism: 'PLAIN',
@@ -31,31 +49,26 @@ var client = new XMPP({
 });
 
 //initialize discord.js
-var discord = new Discord.Client({autoreconnect: true});
+var discord = new Discord.Client({autoReconnect: true});
 
 //auth with the Discord API with the above token
-discord.loginWithToken(DISCORD_API_TOKEN);
+discord.login(DISCORD_API_TOKEN);
 
-//report that discord is connected
-discord.on('ready', function(){
-	console.log("Successfully connected to Discord.");
-});
-
-//report that XMPP is connected, and other things
-client.on('online', function() {
-	console.log("Successfully connected to XMPP chatroom '" + ROOM_JID + '\'.');
-
-	//let the world know we're online, and update our status accordingly
-	client.send(new XMPP.Stanza('presence', {type: 'available'}));
-
-	//join the Multi-User Chatroom
-	client.send(new XMPP.Element('presence', {from: JID, to: ROOM_JID + '/' + IN_CHANNEL_BOT_NAME}));
-});
-
-//discord channel object representing our chatroom
+//discord Channel object representing our chatroom
 var channel = new Discord.Channel({
 	id: DISCORD_CHATROOM_ID,
 	client: "bot"
+});
+
+//Discord debug
+if (DEBUG) discord.on('debug', function(msg){ addLog("[Discord] " + msg) })
+				  .on('warn', function(msg){ addLog("[Discord] " + msg) });
+
+//report that discord is connected
+discord.on('ready', function(){
+	discord.channel = discord.channels.first();
+	console.log("Successfully connected to Discord.");
+	if (DEBUG) addLog("[Discord] Successfully connected to Discord.");
 });
 
 //handle messages from discord server
@@ -68,15 +81,15 @@ discord.on("message", function(message) {
 	var content = new String (message.content);
 	var isMe = false; //is this message using a '/me'
 
-  //handle messages with mentions
-  if (message.mentions.length){
-  	message.mentions.forEach(function(mention){
-  		content = content.replace(
-  			new RegExp('<@(!|)' + mention.id + '>', 'g'),
-  			'@' + mention.username
-  			);
-  	});		
-  }
+	//handle messages with mentions
+	if (message.mentions.length){
+		message.mentions.forEach(function(mention){
+			content = content.replace(
+				new RegExp('<@(!|)' + mention.id + '>', 'g'),
+				'@' + mention.username
+				);
+		});		
+	}
 
 	//handle Discord '/me' command -- its represented by an underscore at the start and end of the content
 	if (message.content[0] == '_' && message.content[message.content.length - 1] == '_'){		
@@ -86,7 +99,7 @@ discord.on("message", function(message) {
 	
 	if (message.attachments.length){
 		TinyURL.shorten(message.attachments[0].url,function(res){
-			client.send(new XMPP.Stanza('message', { to: ROOM_JID, type: 'groupchat', id: RANDOM_ID })
+			xmpp.send(new XMPP.Stanza('message', { to: ROOM_JID, type: 'groupchat', id: RANDOM_ID })
 				.c('body')
 				.t(message.author.username + 
 					(isMe ? ' ' : ': ') +
@@ -94,16 +107,40 @@ discord.on("message", function(message) {
 					content));
 		});
 	}else{
-		client.send(new XMPP.Stanza('message', { to: ROOM_JID, type: 'groupchat', id: RANDOM_ID })
+		var message = (isMe ? '* ' : '[') + message.author.username + (isMe ? ' ' : '] ') +	content;
+		xmpp.send(new XMPP.Stanza('message', { to: ROOM_JID, type: 'groupchat', id: RANDOM_ID })
 			.c('body')
-			.t((isMe ? '* ' : '[') + message.author.username + 
-				(isMe ? ' ' : '] ') +		  
-				content));
+			.t(message));
+		if (DEBUG) {
+			addLog('[Message - Discord to XMPP] ' + message);
+			xmppKeepAlivesSent = 0;
+		}
 	}
-
+})
+discord.on("error", function(error) {
+	console.log("\nSomething went wrong with Discord: \n");
+	console.log(error);
+	addLog('[Discord] ' + error);
 });
 
-client.on('stanza', function(stanza) {
+//Login to XMPP server, and join the chatroom
+xmpp.on('online', function() {
+	console.log("Successfully connected to XMPP chatroom '" + ROOM_JID + '\'.');
+	if (DEBUG) addLog("[XMPP] Successfully connected to XMPP chatroom '" + ROOM_JID);
+
+	//let the world know we're online, and update our status accordingly
+	xmpp.send(new XMPP.Stanza('presence', {type: 'available'}));
+
+	//join the Multi-User Chatroom
+	xmpp.send(new XMPP.Element('presence', {from: JID, to: ROOM_JID + '/' + IN_CHANNEL_BOT_NAME}));
+});
+
+xmpp.on('auth', function(){
+	console.log('Successfully authenticated with ' + JID);
+	if (DEBUG) addLog('[XMPP] Successfully authenticated with ' + JID);
+});
+
+xmpp.on('stanza', function(stanza) {
 	//@TODO ignore all history -- kinda dirty, but XMPP keeps throwing chat history stanzas despite telling it otherwise
 	//or I'm a n00b, likely the latter
 	var isHistory = false;
@@ -135,30 +172,56 @@ client.on('stanza', function(stanza) {
 			var message = '_\* **' + sender + '** ' + bodyText + '_';
 		else
 			var message = '**`[' + sender + ']`** ' + bodyText;
-		discord.sendMessage(channel, message); 
+		discord.channel.sendMessage(message);
+
+		if (DEBUG){
+			addLog('[Message - XMPP to Discord] ' + message);
+			xmppKeepAlivesSent = 0;
+		}
 	}
 });
 
-client.on('error', function(e) {
-	console.log("Something went wrong: ");
+xmpp.on('error', function(e) {
+	console.log("\nSomething went wrong with XMPP:\n");
 	console.log(e);
+	if (DEBUG) addLog('[XMPP Error] ' + e);
 });
 
 //XMPP Server keep-alive
 setInterval(function(){
-	client.send(' ');
-}, 30000);
+	xmpp.send(' ');
+	if (DEBUG) xmppKeepAlivesSent++;
+}, 60000);
 
+//XMPP Server keep-alive
+setInterval(function(){
+	xmpp.send(' ');
+
+	if (DEBUG) addLog('[XMPP] ' + xmppKeepAlivesSent + ' keepAlives sent since the last message sent/received by XMPP');
+	if (DEBUG) addLog('[XMPP] ' + ((xmpp.state == 5) ? 'Connection OK' : 'Problem with connection') + ' (State: ' + xmpp.state + ')');
+}, 1800000); //every half hour
 
 //on close, set XMPP status to offline and logout of discord
 process.on('SIGINT', function(code) {
-	console.log('shutting down')
-	var stanza = new XMPP.Stanza('presence', {from: JID, to: ROOM_JID, type: 'unavailable'})
-	client.send(stanza)
-
-	discord.logout();
-
+	console.log('\nShutting down...');
+	if (DEBUG) addLog('Bot shutting down');
+	xmpp.send(new XMPP.Stanza('presence', {from: JID, to: ROOM_JID, type: 'unavailable'}));
+	discord.destroy();
 	setTimeout(function() {
 		process.exit()
 	}, 1000);
 });
+
+
+//Debug helpers
+function formatLogEntry(inputString){
+	if (inputString.length) return '\n' + Date() + ': ' + inputString;
+}
+
+function addLog(inputString){
+	if (fs){
+		fs.appendFile(LOG_FILE, formatLogEntry(inputString), function(err){
+			if (err) console.log(formatLogEntry('\nCouldn\'t write to the log file for some reason:\n' + err));
+		});
+	}
+}
